52b0a2622dd0c5f9f425461780526a6914144be7
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-11-10 08:17:40 +0500

    thankyou design completed

--- src/components/ThankYou.tsx
+++ src/components/ThankYou.tsx
@@ -1,8 +1,10 @@
 
 const ThankYou = () => {
     return (
-        <div className="info-form">
-        <h1>ThankYou Form</h1>
+        <div className="h-full flex flex-col gap-4 justify-center items-center">
+            <img src="./assets/images/icon-thank-you.svg" alt="thank-you" />
+            <h1 className="font-bold text-3xl">Thank you!</h1>
+            <p className="text-center text-coolGray">Thanks for confirming your subscription! We hope you have fun using our platform, if you ever need support, please feel free to email us at support@loremgaming.com</p>
         </div>
     );
 }
